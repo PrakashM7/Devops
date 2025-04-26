3b7b91665e2a58433102ccca94590d851a8569d6
PrakashM7 <mariyappanprakash17@gmail.com>
2025-04-26 12:37:37 +0530

    creating security group for the instance uisng terraform

---
+++ Terraform/security_group.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "sg_terraform" {
+  name        = "Terraform-test-security-group"
+  description = "Allow SSH and HTTP access"
+  
+  // Ingress (Inbound) rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH access from anywhere
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP access from anywhere
+  }
+
+  // Egress (Outbound) rule - Allow all outbound traffic by default
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # All traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
